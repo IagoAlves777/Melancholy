9fa7e59578411237181ae45e28d6a01db6c29d36
IagoAlves777 <iagox.alves@gmail.com>
2021-04-24 13:38:55 -0300

    correçao frases

--- fase1/scipt1.js
+++ fase1/scipt1.js
@@ -4,18 +4,25 @@ function enviar(){
     if(resposta.value == "desistir"){
         alert("Você acertou")
     }else{
-        if(i>2){
+        if(resposta.value == ""){
+            alert("Resposta inválida!")
+        }
+        else if(i>2){
             alert("O caminho para o sucesso é não DESISTIR") 
         }else if(i==0){
-            alert("O sucesso é ir de fracasso em fracasso sem perder o entusiasmo.")    
+            alert("O sucesso é ir de fracasso em fracasso sem perder o entusiasmo.")
+            i=i+1;    
         }
         else if(i==1){
-            alert("Cada fracasso ensina ao homem algo que ele precisava aprender.")    
+            alert("O fracasso e o sucesso são impostores. Ninguém fracassa tanto como imagina. Ninguém tem tanto sucesso como imagina.")
+            i=i+1;    
         }
         else if(i==2){
-            alert("O sucesso é construído de 99 por cento de fracasso.")    
+            alert("O sucesso é construído de 99 por cento de fracasso.")
+            i=i+1;    
         }
+        
     }
     resposta.value = ("");
-    i=i+1;
+    
 }
